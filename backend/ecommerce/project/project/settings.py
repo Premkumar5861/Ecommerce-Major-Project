8612ea93c7d1df2590598b741e550679b7ae72c7
Premkumar <bosspremkumar5861@gmail.com>
2026-06-16 16:34:21 +0530

    Update settings.py

--- backend/ecommerce/project/project/settings.py
+++ backend/ecommerce/project/project/settings.py
@@ -255,3 +255,16 @@ CORS_ALLOWED_ORIGINS = [
     "https://ecommerce-major-project-n2om.vercel.app",
     "http://localhost:3000",
 ]
+
+# ========== CLOUDINARY CONFIG ==========
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
