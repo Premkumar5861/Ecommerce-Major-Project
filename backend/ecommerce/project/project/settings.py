40e0170a9caf8b4094438303e961b02d7c170c1a
Premkumar <bosspremkumar5861@gmail.com>
2026-06-19 14:54:24 +0530

    fix: add postgresql database config

--- backend/ecommerce/project/project/settings.py
+++ backend/ecommerce/project/project/settings.py
@@ -167,11 +167,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
